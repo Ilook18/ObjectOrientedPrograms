5f603982a2f37c302a4abf3fa03f9349f1dd0c4d
“Enter <anirudhanand984@gmail.com>
2021-11-23 22:54:15 +0530

    [Anirudh] add : creted object oriented programs

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+namespace ObjectOrientedProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Welcome to Object Oriented Programs");
+              
+              
+        }
+    }
+}
